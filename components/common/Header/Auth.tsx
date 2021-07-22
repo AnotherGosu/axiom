760c26851d041e8a7a2ae6d177aaa1d299abe8b7
Maxim Dubinin <anothergosu@gmail.com>
2021-07-22 20:35:51 +1000

    Wrapping login button in skeleton

--- components/common/Header/Auth.tsx
+++ components/common/Header/Auth.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@chakra-ui/skeleton";
 import ButtonLink from "../ButtonLink";
 import ProfileMenu from "./ProfileMenu";
 import { useUser } from "@auth0/nextjs-auth0";
@@ -5,15 +6,18 @@ import { useUser } from "@auth0/nextjs-auth0";
 export default function Auth() {
   const { user, isLoading, error } = useUser();
 
-  if (isLoading) return <div />;
   if (error) {
     console.log(error);
     return <div />;
   }
 
-  return !isLoading && user ? (
-    <ProfileMenu />
-  ) : (
-    <ButtonLink href="/api/auth/login">Войти</ButtonLink>
+  return (
+    <Skeleton isLoaded={!isLoading}>
+      {user ? (
+        <ProfileMenu />
+      ) : (
+        <ButtonLink href="/api/auth/login">Войти</ButtonLink>
+      )}
+    </Skeleton>
   );
 }
